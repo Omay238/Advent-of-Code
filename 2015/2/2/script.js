a4b258d54cd5b34d4c627d1aaa418ce50f73e2fd
Omay <70179858+Omay238@users.noreply.github.com>
2022-11-17 23:28:20 +0000

    new sort

--- 2015/2/2/script.js
+++ 2015/2/2/script.js
@@ -5,7 +5,7 @@ fetch("../data.txt")
     var splitData = data.split("\n");
     for(var i = 0; i < splitData.length; i++){
         var split2 = splitData[i].split("x");
-        split2 = split2.sort((a, b) => a>b?-1:1);
+        split2 = split2.sort((a, b) => a - b);
         console.log(split2);
         ft += split2[0]*2 + split2[1]*2 + split2[0]*split2[1]*split2[2];
     }
