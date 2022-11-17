313c9d7477732f3af0318a5c61083367aebfec8c
Omay <70179858+Omay238@users.noreply.github.com>
2022-11-17 22:48:38 +0000

    2015 day 1 part 1

---
+++ 2015/1/1/script.js
@@ -0,0 +1,13 @@
+fetch("./data.txt")
+.then(res => res.text())
+.then((data) => {
+    var pos = 0;
+    for(var i = 0; i < data.length; i++){
+        if(data.charAt(i) === "("){
+            pos++;
+        }else{
+            pos--;
+        }
+    }
+    document.querySelector("#out").innerText = pos;
+});
