42069bbd370d31ebdf68d524f5272e72f68594a8
Omay <70179858+Omay238@users.noreply.github.com>
2022-11-17 23:11:19 +0000

    day 2

---
+++ 2015/2/2/script.js
@@ -0,0 +1,12 @@
+fetch("../data.txt")
+.then(res => res.text())
+.then((data) => {
+    var ft = 0;
+    var splitData = data.split("\n");
+    for(var i = 0; i < splitData.length; i++){
+        var split2 = splitData[i].split("x");
+        split2 = split2.sort((a, b) => a>b?-1:1);
+        ft += split2[0]*2 + split2[1]*2 + split2[0]*split2[1]*split2[2];
+    }
+    document.querySelector("#out").innerText = ft;
+});
