d5e351e6be7e9a063c4bff2e0d00985ac8d16b8b
Omay <70179858+Omay238@users.noreply.github.com>
2022-11-19 00:52:25 +0000

    remove express

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-const express = require('express');
-const nunjucks = require("nunjucks");
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send(nunjucks.render("index.njk"));
-});
-app.get('/:year', (req, res) => {
-    res.send(nunjucks.render("year.njk", req.params));
-});
-app.get('/:year/:day', (req, res) => {
-    res.send(nunjucks.render("day.njk", req.params));
-});
-app.get('/:year/:day/:part', (req, res) => {
-    res.send(nunjucks.render("part.njk", req.params));
-});
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+const http = require('http');
+const http = require('http');
+const http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World!');
+    res.end();
+}).listen(8080);
