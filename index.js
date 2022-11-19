2ed0d33dc0f81d7e65b840902af52d8a308d3ae5
Omay <70179858+Omay238@users.noreply.github.com>
2022-11-19 03:21:05 +0000

    funny

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const http = require('http');
 const mime = require('mime');
+const fs = require("fs");
 const nunjucks = require('nunjucks');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
-    res.end();
+    if(req.url === "/"){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(nunjucks.render("index.njk"));
+        res.end();
+    }else if(req.url.split("/").length === 2){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(nunjucks.render("year.njk", {path:req.url}));
+        res.end();
+    }else if(req.url.split("/").length === 3){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(nunjucks.render("day.njk", {path:req.url}));
+        res.end();
+    }else if(req.url.split("/").length === 4){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(nunjucks.render("part.njk", {path:req.url}));
+        res.end();
+    }else if(mime.getType(req.url) !== null){
+        res.writeHead(200, {'Content-Type': mime.getType(req.url)});
+        res.write(fs.readFileSync("."+req.url));
+        res.end();
+    }else{
+        console.log(req.url.split("/").length);
+    }
 }).listen(8080);
