142b9ba8e59439237aa68b34217eaf9ac06097bf
Omay <70179858+Omay238@users.noreply.github.com>
2022-11-28 02:06:02 +0000

    format code

--- 2015/1/2.js
+++ 2015/1/2.js
@@ -1,3 +1,16 @@
-document.querySelector("#run").onclick = function(){
-    var data = document.querySelector("#input").value;var floor = 0;for(var i = 0; i < data.length; i++){    if(data.charAt(i) === ")"){        floor--;    }else{        floor++;    }    if(floor === -1){        document.querySelector("#out").innerHTML = i+1;        break;    }}
+document.querySelector("#run").onclick = function() {
+    var data = document.querySelector("#input").value;
+    var split = data.split("\n");
+    var sqft = 0;
+    for (var i = 0; i < split.length; i++) {
+        var t = split[i].split("x").map(x => parseInt(x));
+        var l = t[0];
+        var w = t[1];
+        var h = t[2];
+        var s1 = l * w;
+        var s2 = w * h;
+        var s3 = l * h;
+        sqft += 2 * s1 + 2 * s2 + 2 * s3 + Math.min(s1, s2, s3);
+    }
+    document.querySelector("#out").innerHTML = sqft;
 }
--- 2015/2/2.js
+++ 2015/2/2.js
@@ -1,3 +1,16 @@
-document.querySelector("#run").onclick = function(){
-    var data = document.querySelector("#input").value;var split = data.split("\n");var ft = 0;for(var i = 0; i < split.length; i++){    var t = split[i].split("x").map(x => parseInt(x));    var l = t[0];    var w = t[1];    var h = t[2];    ft += l * w * h + l * 2 + w * 2 + h*2 - Math.max(l * 2, w * 2, h *2);}document.querySelector("#out").innerHTML = ft;
+document.querySelector("#run").onclick = function() {
+    var data = document.querySelector("#input").value;
+    var split = data.split("\n");
+    var sqft = 0;
+    for (var i = 0; i < split.length; i++) {
+        var t = split[i].split("x").map(x => parseInt(x));
+        var l = t[0];
+        var w = t[1];
+        var h = t[2];
+        var s1 = l * w;
+        var s2 = w * h;
+        var s3 = l * h;
+        sqft += 2 * s1 + 2 * s2 + 2 * s3 + Math.min(s1, s2, s3);
+    }
+    document.querySelector("#out").innerHTML = sqft;
 }
